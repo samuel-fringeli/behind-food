66198d82aec820bf7b43a0ea0654cbb441ca990e
Geinoz Grégory <gregory.geinoz@edu.hefr.ch>
2021-11-16 13:27:28 +0100

    First simplest implementation only using WebViews, working well. It will be important to add a CSS file to manage the different sizes of all platforms, now that the app is crossplatform. Next step will be a v1 with the webapp (.html file) running locally, Cordova's style.

---
+++ code/v0/BehindFood/www/js/index.js
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    openBrowser();
+}
+
+function openBrowser() {
+   var url = 'https://adelente-admin.samf.me/app';
+   var target = '_blank';
+   var target2 = '_system';
+   var options = "location = no, toolbar = no";
+   if (device.platform === 'Mac OS X') {
+        var ref = cordova.InAppBrowser.open(url, target2, 'location=no,toolbar=no');
+    }else{
+        var ref = cordova.InAppBrowser.open(url, target, 'location=no,toolbar=no');
+    }
+   
+   ref.addEventListener('loadstart', loadstartCallback);
+   ref.addEventListener('loadstop', loadstopCallback);
+   ref.addEventListener('loaderror', loaderrorCallback);
+   ref.addEventListener('exit', exitCallback);
+
+   function loadstartCallback(event) {
+      console.log('Loading started: '  + event.url)
+   }
+
+   function loadstopCallback(event) {
+      console.log('Loading finished: ' + event.url)
+   }
+
+   function loaderrorCallback(error) {
+      console.log('Loading error: ' + error.message)
+   }
+
+   function exitCallback() {
+      console.log('Browser is closed...')
+   }
+}
